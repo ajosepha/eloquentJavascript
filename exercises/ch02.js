7b1f76b0f6cfa0a2f5dc9e6e55ed826253dbcea3
ajosepha <ariel.isaacs@gmail.com>
2014-11-02 10:43:57 +0200

    looping 1

---
+++ exercises/ch02.js
@@ -0,0 +1,9 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+var line = ''
+var counter = 0
+while(counter < 8){
+  line += '#'
+  counter = counter + 1
+  console.log(line)
+}
